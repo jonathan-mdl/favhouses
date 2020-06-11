98943fb2bfc769ba6001b2013c276d4aca894f27
Jonathan <jmedel.7@gmail.com>
2020-06-10 22:14:51 -0400

    requirements and database changed

--- favhomes/settings.py
+++ favhomes/settings.py
@@ -75,20 +75,20 @@ WSGI_APPLICATION = 'favhomes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-import dj_database_url 
-from decouple import config
-
-DATABASE = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+# import dj_database_url 
+# from decouple import config
+
+# DATABASE = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
